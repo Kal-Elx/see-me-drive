6db7892b4be704835daf8272693ba1273f127afa
Kalle Johansson <kallej1johansson@gmail.com>
2020-10-15 17:28:02 +0200

    feat: highlight player on pause

--- See Me Drive/scripts/Pause.gd
+++ See Me Drive/scripts/Pause.gd
@@ -2,15 +2,19 @@ extends CanvasLayer
 
 export var sec_to_show_count_down = 0.5
 export var sec_to_pause = 3
+export var max_pause_outline = 8.0
+export var min_pause_outline = 2.0
+export var outline_speed = 12
 
-onready var player = get_parent().get_node("Player")
-onready var pause_label = get_child(0)
+onready var player_sprite = get_owner().find_node("Player").get_child(1)
 onready var count_down = load("res://scenes/PauseCountDown.tscn").instance()
 
 var touching = false
 var time_since_touched = 0
 var paused = true
 var counting_down = false
+var curr_pause_outline = min_pause_outline
+var outline_dir = 1
 
 	
 func _process(delta):
@@ -25,6 +29,9 @@ func _process(delta):
 			_show_pause_count_down()
 		else:
 			time_since_touched += delta
+			
+	if paused:
+		_highlight_player(delta)
 	
 	
 func _unhandled_input(event):
@@ -38,27 +45,35 @@ func _unhandled_input(event):
 
 func _resume():
 	get_tree().paused = false
-	pause_label.hide()
 	paused = false
 	_hide_pause_count_down()
-	print("resume")
+	_stop_highlighting_player()
 	
 
 func _pause():
 	get_tree().paused = true
-	pause_label.show()
 	paused = true
 	_hide_pause_count_down()
-	print("pause")
 	
 
 func _show_pause_count_down():
 	add_child(count_down)
 	count_down.count_to(sec_to_pause - sec_to_show_count_down)
 	counting_down = true
-	print("count down")
 	
 	
 func _hide_pause_count_down():
 	counting_down = false
 	remove_child(count_down)
+
+
+func _highlight_player(delta):
+	curr_pause_outline += delta * outline_speed * outline_dir
+	player_sprite.material.set_shader_param("outline_width", curr_pause_outline)
+	if curr_pause_outline > max_pause_outline or curr_pause_outline < min_pause_outline:
+		outline_dir *= -1
+	
+	
+func _stop_highlighting_player():
+	player_sprite.material.set_shader_param("outline_width", 0.0)
+	curr_pause_outline = min_pause_outline
